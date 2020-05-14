4009bff4ecc9e97609f38979b3583f08d10b611b
Leonid <lifeisgym@gmail.com>
2020-05-14 10:55:17 +0300

    Добавить в карточку группы рукописные линии

--- client/src/components/GroupsPage/GroupCard.jsx
+++ client/src/components/GroupsPage/GroupCard.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Card, Pane, Heading, Paragraph, defaultTheme, Icon, Avatar} from 'evergreen-ui'
+import {ReactRough, Line} from "react-rough";
 
 class GroupCard extends Component {
 
@@ -14,6 +15,7 @@ class GroupCard extends Component {
             marginRight={25}
             marginBottom={25}>
             <Pane
+                position='relative'
                 paddingTop={24}
                 paddingBottom={16}
                 paddingX={25}
@@ -27,24 +29,41 @@ class GroupCard extends Component {
                 </Heading>
                 <Paragraph
                     size={400}
+                    marginBottom={10}
                     overflow='hidden'
                     color='muted'
                     textOverflow='ellipsis'>
                     {data.description}
                 </Paragraph>
+                <Pane
+                    position='absolute'
+                    bottom={-8}
+                    left={25}>
+                    <ReactRough
+                        width={300}
+                        height={10}>
+                        <Line
+                            x1={0}
+                            x2={300}
+                            y1={5}
+                            y2={5}
+                            stroke={defaultTheme.palette.blue.base}
+                            roughness={1.7}
+                        />
+                    </ReactRough>
+                </Pane>
             </Pane>
             <Pane
-                paddingTop={15}
+                paddingTop={25}
                 paddingBottom={20}
                 paddingX={25}>
                 <Pane
+                    position='relative'
                     display='flex'
-                    borderColor={defaultTheme.colors.border.muted}
-                    borderBottom='1px solid'
-                    marginBottom={20}>
-                    {!!data.users.length && <Pane
-                        flexGrow={1}
-                        marginBottom={16}>
+                    paddingBottom={25}
+                    marginBottom={30}>
+                    <Pane
+                        flexGrow={1}>
                         <Pane
                             display='flex'
                             paddingTop={1}
@@ -67,10 +86,9 @@ class GroupCard extends Component {
                                 name={user.name}
                             />
                         })}
-                    </Pane>}
+                    </Pane>
                     {!!data.invited.length && <Pane
-                        flexGrow={1}
-                        marginBottom={10}>
+                        flexGrow={1}>
                         <Pane
                             display='flex'
                             marginBottom={12}>
@@ -93,6 +111,24 @@ class GroupCard extends Component {
                             />
                         })}
                     </Pane>}
+                    <Pane
+                        position='absolute'
+                        bottom={-10}
+                        left={0}>
+                        <ReactRough
+                            width={300}
+                            height={10}>
+                            <Line
+                                x1={0}
+                                x2={300}
+                                y1={5}
+                                y2={5}
+                                stroke={defaultTheme.colors.border.default}
+                                roughness={0.5}
+                                strokeLineDash={[5,13]}
+                            />
+                        </ReactRough>
+                    </Pane>
                 </Pane>
                 <Pane>
                     <Pane
